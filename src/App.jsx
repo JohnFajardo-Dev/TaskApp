c935659c9cd2c1de25acc791dbbc00c634a6e2d9
John Fajardo <62899394+Fajardo-dev@users.noreply.github.com>
2024-07-20 14:38:38 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 // Pages
-import Home from "./pages/Home/Home";
 import Tasks from "./pages/Tasks/Tasks";
 import Calendar from "./pages/Calendar/Calendar";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/tasks" element={<Tasks />} />
+      <Route path="/" element={<Tasks />} />
       <Route path="/calendar" element={<Calendar />} />
     </Routes>
   );
--- src/modules/NavBar/NavBar.tsx
+++ src/modules/NavBar/NavBar.tsx
@@ -4,7 +4,6 @@ import "./NavBar.css"
 // Icons
 import { IconContext } from "react-icons";
 import {FaTasks} from "react-icons/fa"
-import { GoHomeFill } from "react-icons/go";
 import { FaCalendarAlt } from "react-icons/fa";
 import { IoSettingsSharp} from "react-icons/io5";
 
@@ -13,10 +12,7 @@ function NavBar() {
     <IconContext.Provider value={{size:"2rem", color:"#000"}}>
     <nav>
         <ul>
-            <li><Link to="/"><GoHomeFill /></Link></li>
-            <li><Link to="/tasks"><FaTasks/></Link></li>
-            <li><Link to="/calendar"><FaCalendarAlt /></Link></li>
-            <li><Link to="/settings"><IoSettingsSharp /></Link></li>
+            <li><Link to="/"><FaTasks/></Link></li>
         </ul>
     </nav>
         </IconContext.Provider>
