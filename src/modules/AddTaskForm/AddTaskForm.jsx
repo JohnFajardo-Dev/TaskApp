cc334b47f5d7670e2a81d0ddfebc71f1dc7a177d
Fajardo-dev <johnfajardo177@gmail.com>
2024-07-25 15:12:59 -0500

    Update

--- src/modules/AddTaskForm/AddTaskForm.jsx
+++ src/modules/AddTaskForm/AddTaskForm.jsx
@@ -25,46 +25,57 @@ function AddTaskForm({ setTasks, tasks }) {
       setTasks(updatedTasks);
       localStorage.setItem("tasks", JSON.stringify(updatedTasks));
       setNewTaskName(""); // Limpiar el campo de texto después de agregar la tarea
-      setTaskColor("yellow"); // Reiniciar el color al valor por defecto (opcional)
+      setTaskColor(""); // Reiniciar el color al valor por defecto (opcional)
     }
   };
 
   return (
     <form className="add-task-form" onSubmit={handleSubmit}>
       <h2>Add your Task</h2>
-      <input
-        type="date"
-        value={newDate}
-        onChange={(event) => setNewDate(event.target.value)}
-        placeholder="Date"
-        name="date-input"
-        required
-      />
-      <input
-        type="time"
-        onChange={(event) => setNewTime(event.target.value)}
-        placeholder={"Time"}
-        value={newTime}
-        required
-      />
-      <input
-        type="text"
-        value={newTaskName}
-        onChange={(event) => setNewTaskName(event.target.value)}
-        placeholder="Title"
-        required
-      />
-      <select
-        value={taskColor}
-        onChange={(event) => setTaskColor(event.target.value)}
-        required
-      >
-        <option value="yellow">Yellow</option>
-        <option value="black-blue">Black Blue</option>
-        <option value="white-blue">White Blue</option>
-        <option value="black">Black</option>
-        <option value="red">Red</option>
-      </select>
+      <div>
+        <label htmlFor="">Task Title</label>
+        <input
+          type="text"
+          value={newTaskName}
+          onChange={(event) => setNewTaskName(event.target.value)}
+          placeholder="Title"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="">Task Date</label>
+        <input
+          type="date"
+          value={newDate}
+          onChange={(event) => setNewDate(event.target.value)}
+          placeholder="Date"
+          name="date-input"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="">Task Time</label>
+        <input
+          type="time"
+          onChange={(event) => setNewTime(event.target.value)}
+          placeholder={"Time"}
+          value={newTime}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="">Color Target Task</label>
+        <select
+          value={taskColor}
+          onChange={(event) => setTaskColor(event.target.value)}
+          required
+        >
+          <option value="black-blue">Black Blue</option>
+          <option value="white-blue">White Blue</option>
+          <option value="black">Black</option>
+          <option value="red">Red</option>
+        </select>
+      </div>
       <button type="submit">Add Task</button>
     </form>
   );
--- src/pages/Tasks/Tasks.tsx
+++ src/pages/Tasks/Tasks.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import "./Tasks.css";
-import NavBar from '../../modules/NavBar/NavBar';
 import TaskList from '../../modules/TaskList/TaskList';
-import AddTaskForm from '../../modules/AddTaskForm/AddTaskForm';
 
 function getTasksFromLocalStorage() {
   const tasksJSON = localStorage.getItem('tasks');
